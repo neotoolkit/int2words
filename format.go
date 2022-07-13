22051ed4753ba7fb9be9060b4443a8f06903ef3a
Sasha Melentyev <sasha@melentyev.io>
2022-07-13 09:13:06 +0300

    refactor(format): reuse SetFormat

--- format.go
+++ format.go
@@ -6,46 +6,38 @@ import (
 )
 
 func SetUpperCamelCaseFormat() Option {
-	return func(opts *Options) {
-		opts.format = func(words []string) string {
-			for i, v := range words {
-				word := []rune(v)
-				word[0] = unicode.ToTitle(word[0])
-				words[i] = string(word)
-			}
-			return strings.Join(words, "")
+	return SetFormat(func(words []string) string {
+		for i, v := range words {
+			word := []rune(v)
+			word[0] = unicode.ToTitle(word[0])
+			words[i] = string(word)
 		}
-	}
+		return strings.Join(words, "")
+	})
 }
 
 func SetLowerCamelCaseFormat() Option {
-	return func(opts *Options) {
-		opts.format = func(words []string) string {
-			for i, v := range words {
-				if i == 0 {
-					continue
-				}
-				word := []rune(v)
-				word[0] = unicode.ToTitle(word[0])
-				words[i] = string(word)
+	return SetFormat(func(words []string) string {
+		for i, v := range words {
+			if i == 0 {
+				continue
 			}
-			return strings.Join(words, "")
+			word := []rune(v)
+			word[0] = unicode.ToTitle(word[0])
+			words[i] = string(word)
 		}
-	}
+		return strings.Join(words, "")
+	})
 }
 
 func SetSnakeCaseFormat() Option {
-	return func(opts *Options) {
-		opts.format = func(words []string) string {
-			return strings.Join(words, "_")
-		}
-	}
+	return SetFormat(func(words []string) string {
+		return strings.Join(words, "_")
+	})
 }
 
 func SetKebabCaseFormat() Option {
-	return func(opts *Options) {
-		opts.format = func(words []string) string {
-			return strings.Join(words, "-")
-		}
-	}
+	return SetFormat(func(words []string) string {
+		return strings.Join(words, "-")
+	})
 }
